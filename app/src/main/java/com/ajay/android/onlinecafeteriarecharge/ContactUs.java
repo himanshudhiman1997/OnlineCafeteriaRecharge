adb59b34e99bdb0414bbf0f20bcd67f2aa3fe4a7
Himanshu Dhiman <himanshudhimanhr@gmail.com>
2018-06-21 15:06:39 +0530

    v

--- app/src/main/java/com/ajay/android/onlinecafeteriarecharge/ContactUs.java
+++ app/src/main/java/com/ajay/android/onlinecafeteriarecharge/ContactUs.java
@@ -2,6 +2,7 @@ package com.ajay.android.onlinecafeteriarecharge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class ContactUs extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class ContactUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+
+        VideoView videoView = findViewById(R.id.video);
+        videoView.start();
     }
 }
